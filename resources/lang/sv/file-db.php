c2ce7821aa8f18c82704ce0bdba698e7c166e1ee
Saed Yousef <saed.alzaben@gmail.com>
2025-11-19 12:51:34 +0100

    Add Swedish language file for file database

---
+++ resources/lang/sv/file-db.php
@@ -0,0 +1,38 @@
+<?php
+
+
+return [
+    'upload' => 'Ladda upp',
+    'new-folder' => 'Ny mapp',
+    'sync' => 'Uppdatera',
+    'cancel' => 'Avbryt',
+    'close' => 'Stäng',
+    'delete' => 'Radera',
+    'caution' => 'Varning',
+    'delete-file' => 'Radera fil',
+    'delete-dir' => 'Radera katalog',
+    'new-folder-inline' => 'Skapa',
+    'actions-title' => 'Åtgärd',
+    'upload-file' => 'Ladda upp fil',
+    'messages' => [
+        'folder-empty' => 'Mappen är tom',
+        'upload-failed' => 'Filen kunde inte sparas. Behörigheter?',
+        'uploaded-file-missing' => 'Ingen fil laddades upp',
+        'dirname-missing' => 'Vänligen ange ett mappnamn',
+        'file-has-following-dependencies' => 'Filen används i följande sammanhang:',
+        'really-delete' => 'Vill du verkligen radera filen?',
+        'really-delete-dir' => 'Vill du verkligen radera katalogen?',
+        'cannot-delete-nonempty-dir' => 'En katalog måste vara tom innan den kan raderas',
+        'delete-failed' => 'Filen/mappen kunde inte raderas',
+    ],
+    'file-properties' => [
+        'name' => 'Fil',
+        'dir-name' => 'Mappnamn',
+        'updated_at' => 'Datum'
+    ],
+    'dependency-properties' => [
+        'id' => 'ID',
+        'category' => 'Kategori',
+        'title' => 'Beskrivning'
+    ],
+];
