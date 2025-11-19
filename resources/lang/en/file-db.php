9986ac4996cb65e469ac55b5a36466a714a3c69f
Saed Yousef <saed.alzaben@gmail.com>
2025-11-19 12:53:02 +0100

    Add English language file for file database messages

---
+++ resources/lang/en/file-db.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'upload'                => 'Upload',
+    'new-folder'            => 'New Folder',
+    'sync'                  => 'Refresh',
+    'cancel'                => 'Cancel',
+    'close'                 => 'Close',
+    'delete'                => 'Delete',
+    'caution'               => 'Caution',
+    'delete-file'           => 'Delete File',
+    'delete-dir'            => 'Delete Directory',
+    'new-folder-inline'     => 'Create',
+    'actions-title'         => 'Action',
+    'upload-file'           => 'Upload File',
+    'messages'  => [
+        'folder-empty'                      => 'The folder is empty',
+        'upload-failed'                     => 'The file could not be saved. Permissions issue?',
+        'uploaded-file-missing'             => 'No file was uploaded',
+        'dirname-missing'                   => 'Please provide a folder name',
+        'file-has-following-dependencies'   => 'The file is used in the following context:',
+        'really-delete'                     => 'Do you really want to delete this file?',
+        'really-delete-dir'                 => 'Do you really want to delete this directory?',
+        'cannot-delete-nonempty-dir'        => 'A directory must be empty before it can be deleted',
+        'delete-failed'                     => 'The file/folder could not be deleted',
+    ],
+    'file-properties' => [
+        'name'          => 'File',
+        'dir-name'      => 'Folder Name',
+        'updated_at'    => 'Date'
+    ],
+    'dependency-properties' => [
+        'id'            => 'ID',
+        'category'      => 'Category',
+        'title'         => 'Description'
+    ],
+];
